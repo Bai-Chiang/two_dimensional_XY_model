f056e30a60b48e3089ab5a3d02718f48600af1ad
Baiqiang <qiangbq@gmail.com>
2017-06-06 14:56:35 +0800

    current energy completed

--- main.cu
+++ main.cu
@@ -41,41 +41,67 @@ void initialize (float* spins, curandState_t* states) {
  * the total warm_up_step for the whole system is n_itter * n_block * n_threads_per_block
  */
 __global__ 
-void warm_up (float* spins, int* p_length, long long* n_itter) {
-    int dim1 = 0;
-    int dim2 = 0;
+void warm_up (float* spins, int* p_length, long long* p_n_itter, curandState_t* states) {
+    long long n_itter = *p_n_itter;
     int length = *p_length;
-    float upper_spin = 0, lower_spin = 0, left_spin = 0, right_spin = 0;
+    int index = blockIdx.x *blockDim.x + threadIdx.x;
+    int dim1, dim2;
+    float upper_spin, lower_spin, left_spin, right_spin;
+    float current_energy, changed_energy;
+    float new_spin, current_spin;
+    for (long long i = 0; i < n_itter; ++i){
+        dim1 = 0;
+        dim2 = 0;
 
-    // get upper spin
-    if (dim1 == 0) {
-        upper_spin = spins[(length-1) * length + dim2];
-    } else {
-        upper_spin = spins[(dim1-1) * length + dim2];
+        // get current position spin
+        current_spin = spins[dim1 * length + dim2];
+
+        // get upper spin
+        if (dim1 == 0) {
+            upper_spin = spins[(length-1) * length + dim2];
+        } else {
+            upper_spin = spins[(dim1-1) * length + dim2];
+        }
+
+        // get lower spin
+        if (dim1 == length-1) {
+            lower_spin = spins[0 * length + dim2];
+        } else {
+            lower_spin = spins[(dim1+1) * length + dim2];
+        }
+
+        // get left spin
+        if (dim2 == 0) {
+            left_spin = spins[dim1 * length + (length-1)];
+        } else {
+            left_spin = spins[dim1 * length + (dim2-1)];
+        }
+
+        // get right spin
+        if (dim2 == length-1) {
+            right_spin = spins[dim1 * length + 0];
+        } else {
+            right_spin = spins[dim1 * length + (dim2+1)];
+        }
+
+        // get energy
+        current_energy = -(std::cos(current_spin - upper_spin) + std::cos(current_spin - lower_spin)
+                           + std::cos(current_spin - left_spin) + std::cos(current_spin - right_spin));
+
+        // new spin direction
+        new_spin = curand_uniform(&states[index]) * 2*M_PI;
+
+        spins[0] = current_spin;
+        spins[1] = upper_spin;
+        spins[2] = lower_spin;
+        spins[3] = left_spin;
+        spins[4] = right_spin;
+        spins[5] = std::cos(current_spin - upper_spin);
+        spins[6] = std::cos(current_spin - lower_spin);
+        spins[7] = std::cos(current_spin - left_spin);
+        spins[8] = std::cos(current_spin - right_spin);
+        spins[9] = current_energy;
     }
-
-    // get lower spin
-    if (dim1 == length-1) {
-        lower_spin = spins[0 * length + dim2];
-    } else {
-        lower_spin = spins[(dim1+1) * length + dim2];
-    }
-
-    // get left spin
-    if (dim2 == 0) {
-        left_spin = spins[dim1 * length + (length-1)];
-    } else {
-        left_spin = spins[dim1 * length + (dim2-1)];
-    }
-
-    // get right spin
-    if (dim2 == length-1) {
-        right_spin = spins[dim1 * length + 0];
-    } else {
-        right_spin = spins[dim1 * length + (dim2+1)];
-    }
-
-    spins[0] = right_spin;
 }
     
     
@@ -103,8 +129,8 @@ int main() {
     int threads_per_block = std::min(1024LL, size);
     int blocks = size/threads_per_block;
     long long warm_up_steps = 1024;
-    long long n_itter = warm_up_steps/size;
-
+    //long long n_itter = warm_up_steps/size;
+    long long n_itter = 1;
     // --------------------------------------------------------------------------------------------
     curandState_t* states; // used to store random state for each core
     cudaMalloc((void**) &states, size * sizeof(curandState_t)); // allocate memory in device
@@ -131,8 +157,9 @@ int main() {
         initialize <<<blocks, threads_per_block>>> (d_spins, states);
 
         // warm up 
-        warm_up <<<blocks, threads_per_block>>> (d_spins, d_length, d_n_itter);
-        
+        //warm_up <<<blocks, threads_per_block>>> (d_spins, d_length, d_n_itter, states);
+        warm_up <<<1,1>>> (d_spins, d_length, d_n_itter, states);
+
         // copy memory from device to host
         cudaMemcpy(p_spins, d_spins, size * sizeof(float), cudaMemcpyDeviceToHost);
 
